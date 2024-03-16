2adc57ffd3bc13a061ba6fba560a3e3ccacebdb8
Vanced Solutions <145443805+vancedsolutions@users.noreply.github.com>
2024-03-16 12:56:37 +0530

    add admin  route

--- src/app/onboarding/pcr/layout.tsx
+++ src/app/onboarding/pcr/layout.tsx
@@ -1,7 +1,22 @@
+"use client";
 import React from "react";
 import { PCRContextProvider } from "@/context/onboarding/personal.credit.repair.context";
+import { useAuth } from "@/context/guard/guard.context";
+import { TypeofServiceScreen } from "@/screen/service_types/typeof.service.screen";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
 export default function PCRLayout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { isAdmin } = useAuth();
+  const loginRole = Cookies.get("role");
+  if (isAdmin) {
+    router.replace("/client-dashboard");
+  }
+  if (loginRole == "PCR:Admin") {
+    router.replace("/client-dashboard");
+  }
+
   return (
     <PCRContextProvider>
       <div
