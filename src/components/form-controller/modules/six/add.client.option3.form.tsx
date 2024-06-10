df44893b639c2f321975095fe47083b5422fdb72
SHERRY699 <shehryarnasir98@gmail.com>
2024-06-10 23:57:31 +0500

    admin panel

--- src/components/form-controller/modules/six/add.client.option3.form.tsx
+++ src/components/form-controller/modules/six/add.client.option3.form.tsx
@@ -42,8 +42,9 @@ export const AddClientOption3Form = () => {
         Generate the letter to get all credit report
         <EllipsisOutlined  className="text-white bg-[#1380FF] rounded-full"/>
         </div>
+
         <div className="bg-white w-6/12 h-[800px] shadow-md rounded-lg flex flex-col items-center pt-10">
-          <div className="w-[90%] ">
+        <div className="w-[90%] ">
           <Dropdown menu={menuProps} className="w-full h-[80px] flex justify-center gap-10 items-center text-[#686666] border-gray-400 rounded-xl border-[1px]">
         <Button>
         <Space>
@@ -54,15 +55,23 @@ export const AddClientOption3Form = () => {
          </Dropdown>
           </div>
 
-          <div className="W-[90%] h-[80px] flex flex-col ">
-            <div className="card w-[90%] h-[80px] bg-[#F9F9F9] flex justify-between">
-              <h1>#434343</h1>
-            </div>
-            <input type="checkbox" name="equifax" id="equi" />
 
+          <div className="w-[90%]  flex  h-[120px] gap-2 mt-10">
+
+          <div className="card w-full h-[120px] bg-[#F9F9F9] flex flex-col justify-between">
+              <h1>Equifax</h1>
+                <p>Upload you your report</p>
+                <div className="w-[90%] bg-[#F4F4F4] rounded-full h-16">
+                <input type="file" /> 
+                </div>
+                <p>Supported files are PNG,JPG & PDF </p>
           </div>
-
+          <input type="checkbox" name="equifax" id="equi"  className="text-xl"/>
+          </div>
         </div>
+
+
+
        </div>
 
      </section>
