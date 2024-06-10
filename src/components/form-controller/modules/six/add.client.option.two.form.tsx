3ad632be5e77d0fd80d0161da770bb3a9a75a75e
SHERRY699 <shehryarnasir98@gmail.com>
2024-06-10 22:12:52 +0500

    error

--- src/components/form-controller/modules/six/add.client.option.two.form.tsx
+++ src/components/form-controller/modules/six/add.client.option.two.form.tsx
@@ -7,144 +7,169 @@ import { MenuOutlined } from "@ant-design/icons";
 import { HolderOutlined } from "@ant-design/icons";
 import { EditOutlined } from "@ant-design/icons";
 
-
-
-
-
-
-
-
 export const AddClientOptionTwoForm = () => {
-  const [hidden,setHidden]=useState(false)
-  const [hiddden,setHiddden]=useState(true)
+  const [hidden, setHidden] = useState(false);
+  const [hiddden, setHiddden] = useState(true);
 
-  const setHiddenDiv = ( )=>{
-    setHidden(!hidden)
-  }
+  const setHiddenDiv = () => {
+    setHidden(!hidden);
+  };
 
-  const setHidddenDiv = ( )=>{
-    setHiddden(!hiddden)
-  }
- 
+  const setHidddenDiv = () => {
+    setHiddden(!hiddden);
+  };
+
   return (
     <>
-    <h1 className="text-[40px] md:text-[50px] lg:text-[65px] ml-10 xl:ml-0 mt-4 font-bold text-[#686666]">Current Users</h1>
-    <section className=" min-h-screen flex flex-col ml-4 items-start">
-      <div className="buttons w-full h-[100px] flex items-center justify-between ">
-        <div className="1 w-[50%] h-full flex items-center gap-6">
-        <button className="w-[150px] h-[45px] bg-[#EFF6FF] text-[#1380FF] font-bold flex items-center gap-2 px-4"> <span className="text-white bg-[#1380FF] rounded-full font-bold px-1 py-[1px]">12</span> ACTIVE</button>
-        <button className="w-[180px] h-[45px]  text-[#8A8D93] font-bold border-[1px] border-gray-400 flex items-center gap-2 px-4 rounded-lg"> <span className="text-black bg-[#E9E9E9] font-bold rounded-full px-1 py-[1px]">6</span> INACTIVE</button>
-        </div>
-        <div className="2  w-[50%] h-full flex items-center  justify-end  md:justify-center ">
-          <div className="w-full h-full hidden md:flex items-center justify-center gap-4">
-          <input type="text" placeholder="Search By Name" className="w-[250px] h-[40px] px-2 rounded-lg outline-none border-[1px]"/>
-          <button className="bg-[#2684FF] text-white w-[140px] h-[40px] rounded-lg">Add New User</button>
-          <button className="text-[#3A3541] text-xl"><MoreOutlined /></button>
+      <h1 className="text-[40px] md:text-[50px] lg:text-[65px] ml-10 xl:ml-0 mt-4 font-bold text-[#686666]">
+        Current Users
+      </h1>
+      <section className=" min-h-screen flex flex-col ml-4 items-start">
+        <div className="buttons w-full h-[100px] flex items-center justify-between ">
+          <div className="1 w-[50%] h-full flex items-center gap-6">
+            <button className="w-[150px] h-[45px] bg-[#EFF6FF] text-[#1380FF] font-bold flex items-center gap-2 px-4">
+              {" "}
+              <span className="text-white bg-[#1380FF] rounded-full font-bold px-1 py-[1px]">
+                12
+              </span>{" "}
+              ACTIVE
+            </button>
+            <button className="w-[180px] h-[45px]  text-[#8A8D93] font-bold border-[1px] border-gray-400 flex items-center gap-2 px-4 rounded-lg">
+              {" "}
+              <span className="text-black bg-[#E9E9E9] font-bold rounded-full px-1 py-[1px]">
+                6
+              </span>{" "}
+              INACTIVE
+            </button>
           </div>
-          <button className="md:hidden sm:mr-10  "
-        onClick={setHiddenDiv}
+          <div className="2  w-[50%] h-full flex items-center  justify-end  md:justify-center ">
+            <div className="w-full h-full hidden md:flex items-center justify-center gap-4">
+              <input
+                type="text"
+                placeholder="Search By Name"
+                className="w-[250px] h-[40px] px-2 rounded-lg outline-none border-[1px]"
+              />
+              <button className="bg-[#2684FF] text-white w-[140px] h-[40px] rounded-lg">
+                Add New User
+              </button>
+              <button className="text-[#3A3541] text-xl">
+                <MoreOutlined />
+              </button>
+            </div>
+            <button className="md:hidden sm:mr-10  " onClick={setHiddenDiv}>
+              <MenuOutlined />
+            </button>
+          </div>
+        </div>
+
+        <div
+          className={`${
+            hidden ? "absolute" : "hidden"
+          } flex bg-gray-400 rounded-lg top-36 gap-4 right-1 w-[300px] h-max py-2 flex-col items-center justify-center`}
         >
-            <MenuOutlined />
-         </button>
-        </div>
-     
-
-      
-
-      </div>
-      
-       <div className={`${hidden?'absolute':'hidden'} flex bg-gray-400 rounded-lg top-36 gap-4 right-1 w-[300px] h-max py-2 flex-col items-center justify-center`}>
-         <input type="text" placeholder="Search By Name" className="w-[250px] h-[40px] px-2 rounded-lg outline-none border-[1px]"/>
-          <button className="bg-[#2684FF] text-white w-[140px] h-[40px] rounded-lg">Add New User</button>
-          <button className="text-[#3A3541] text-xl"><MoreOutlined /></button>
+          <input
+            type="text"
+            placeholder="Search By Name"
+            className="w-[250px] h-[40px] px-2 rounded-lg outline-none border-[1px]"
+          />
+          <button className="bg-[#2684FF] text-white w-[140px] h-[40px] rounded-lg">
+            Add New User
+          </button>
+          <button className="text-[#3A3541] text-xl">
+            <MoreOutlined />
+          </button>
         </div>
 
 
-      <div className="  w-[90%]  border-[1px] h-[800px] ">
-        <div className='w-full h-12 bg-[#F4F4F4] rounded-2xl'>
-          <div className="w-full h-full flex items-center rounded-xl text-left px-2 text-[12px] text-[#333333]">
-          <div className="w-[13%] h-[12px] border-r-2 border-l-2 border-gray-500 gap-3 flex items-center justify-center">
-          <input type="checkbox" name="chck" id="check" />
-          <p>USERS</p>
+          <div className="w-[300px] xl:w-full h-[800px] overflow-x-auto overflow-y-auto">
+          <div className="w-[1200px] xl:w-[95%]  border-[1px] h-[800px]  ">
+          <div className="w-full h-12 bg-[#F4F4F4] rounded-2xl">
+            <div className="w-full h-full flex items-center rounded-xl text-left px-2 text-[8px] xl:text-[12px] text-[#333333] overflow-y-auto ">
+              <div className="w-[13%] h-[12px] border-r-2 border-l-2 border-gray-500 gap-6 flex items-center justify-center">
+                <input type="checkbox" name="chck" id="check" />
+                <p>USERS</p>
+              </div>
+              <div className="w-[13%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
+                <p>EMAIL</p>
+              </div>
+              <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
+                <p>PASSWORDS</p>
+              </div>
+              <div className="w-[12%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
+                <p>PHONE NUMBER</p>
+              </div>
+              <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
+                <p>CITY</p>
+              </div>
+              <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
+                <p>STATE</p>
+              </div>
+              <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
+                <p>ZIP CODE</p>
+              </div>
+              <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
+                <p>SS NUMBER</p>
+              </div>
+              <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
+                <p>ACTION REQUIRED</p>
+              </div>
+            </div>
           </div>
-          <div className="w-[13%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
-          <p>EMAIL</p>
+
+          <div className="w-full  h-12 flex flex-col">
+            <div className="w-full h-full relative flex items-center  text-left px-2 text-[8px] xl:text-[12px] text-[#737373] border-b-[1px] border-[#737373] ">
+              <div className="w-[13%] flex items-center gap-3  justify-center">
+                <input type="checkbox" name="chck" id="check" />
+                tashanda powell
+              </div>
+              <div className="w-[13%] flex items-center justify-center">
+                brokeralexa@gmail.com
+              </div>
+              <div className="w-[10%] flex items-center justify-center">
+                sadasd
+              </div>
+              <div className="w-[12%] flex items-center justify-center">
+                131312312
+              </div>
+              <div className="w-[10%] flex items-center justify-center">
+                Brooklyn
+              </div>
+              <div className="w-[10%] flex items-center justify-center">
+                New York
+              </div>
+              <div className="w-[10%] flex items-center justify-center">
+                11210
+              </div>
+              <div className="w-[10%] flex items-center justify-center">
+                243296532
+              </div>
+              <div className="w-[10%] flex items-center justify-center text-lg gap-6">
+                <button>
+                  <EditOutlined />
+                </button>
+                <button onClick={setHidddenDiv}>
+                  <HolderOutlined />
+                </button>
+              </div>
+              <div
+                className={`${
+                  hiddden ? "hidden" : "absolute"
+                } top-8 right-6 w-[100px] h-16 bg-white flex flex-col items-center justify-center gap-3 rounded-xl`}
+              >
+                <button className="text-[#00B4D8]">Edit</button>
+                <button className="text-[#EF233C]">Delete</button>
+              </div>
+            </div>
           </div>
-          <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
-          <p>PASSWORDS</p>
+        
           </div>
-          <div className="w-[12%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
-          <p>PHONE NUMBER</p>
-          </div>
-          <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
-          <p>CITY</p>
-          </div>
-          <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
-          <p>STATE</p>
-          </div>
-          <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
-          <p>ZIP CODE</p>
-          </div>
-          <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
-          <p>SS NUMBER</p>
-          </div>
-          <div className="w-[10%] h-[12px] border-r-2 border-l-2 border-gray-500 flex items-center justify-center">
-          <p>ACTION REQUIRED</p>
-          </div>
-          
+
+
 
           </div>
-        </div>
-
-        <div className="w-full  h-12 flex flex-col">
-        <div className="w-full h-full relative flex items-center  text-left px-2 text-[12px] text-[#737373] border-b-[1px] border-[#737373]">
-         <div className="w-[13%] flex items-center gap-3  justify-center">
-         <input type="checkbox" name="chck" id="check" />
-          tashanda powell
-         </div>
-         <div className="w-[13%] flex items-center justify-center">
-          brokeralexa@gmail.com
-         </div>
-         <div className="w-[10%] flex items-center justify-center">
-          sadasd
-         </div>
-         <div className="w-[12%] flex items-center justify-center">
-          131312312
-         </div> 
-         <div className="w-[10%] flex items-center justify-center">
-         Brooklyn
-         </div>
-          <div className="w-[10%] flex items-center justify-center">
-         New York
-         </div>
-          <div className="w-[10%] flex items-center justify-center">
-          11210
-         </div> 
-         <div className="w-[10%] flex items-center justify-center">
-         243296532
-         </div>
-          <div className="w-[10%] flex items-center justify-center text-lg gap-6">
-         <button ><EditOutlined /></button>
-         <button onClick={setHidddenDiv}><HolderOutlined /></button>
-         </div>
-         <div className={`${hiddden?'hidden':'absolute'} top-8 right-6 w-[100px] h-16 bg-white flex flex-col items-center justify-center gap-3 rounded-xl`}>
-            <button className="text-[#00B4D8]">Edit</button>
-            <button className="text-[#EF233C]">Delete</button>
-         </div>
-        </div>
        
-       
 
-        </div>
-
-      </div>
-
-    </section>
-
-  
-  
-  
-  
+      </section>
     </>
   );
 };
--- src/components/form-controller/modules/six/add.client.option3.form.tsx
+++ src/components/form-controller/modules/six/add.client.option3.form.tsx
@@ -1,180 +1,70 @@
 "use client";
 // @ts-ignore
-import useForm from "new-react-use-form";
-import React, { FormEventHandler, useEffect, useLayoutEffect } from "react";
-import { IconButton, InputAdornment, TextField } from "@mui/material";
-import MenuItem from "@mui/material/MenuItem";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers-pro";
-import { DatePicker } from "@mui/x-date-pickers-pro";
-import { useMutation } from "@tanstack/react-query";
-import { ApiCalls } from "@/api/calls/calls";
-import { Calls } from "@/api/calls/type";
-import Autocomplete from "@mui/material/Autocomplete";
-import { states } from "@/data/ states";
-import { QuestionAnswerOutlined, Visibility, VisibilityOff } from "@mui/icons-material";
-import { Checkbox, Typography } from "@material-tailwind/react";
-import { usePCR } from "@/context/onboarding/personal.credit.repair.context";
-import { useAC } from "@/context/business-credit/add-client/personal.credit.repair.context";
-import { handleFormError } from "@/utils/error";
-import toast from "react-hot-toast";
-import Cookies from "js-cookie";
-import { useRouter } from "next/navigation";
-import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
-interface Option {
-  label: string;
-}
+import {EllipsisOutlined } from "@ant-design/icons"
+import type { MenuProps } from 'antd';
+import { Button, Dropdown, message, Space } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+
+
 export const AddClientOption3Form = () => {
-  const router = useRouter();
-  const {
-    SetFormID,
-    SetIsPending,
-    SetButtonText,
-    SetMobileH4,
-    SetMobileText,
-    SetMobileValue,
-  } = useAC();
-  const [showPassword, setShowPassword] = React.useState(false);
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-  const handleMouseDownPassword = (
-    event: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    event.preventDefault();
-  };
 
-  const form = useForm({
-    firstName: "",
-    lastName: "",
-    gender: "",
-    dob: "",
-    ssn: "",
-    cityName: "",
-    zipCode: "",
-    state: "",
-    email: "",
-    phone: "",
-    password: "",
-    cnfPassword: "",
-    policy: null,
-  });
-
-  const { mutateAsync, isPending } = useMutation<
-    Calls.IResponse.ModulesSignUp,
-    Error,
-    Calls.IRequest.ModuleOneSignUp
-  >({
-    mutationFn: (variables) => ApiCalls.Module.one.signUp(variables),
-    onSuccess: (r) => {
-      toast.success(r.message);
-      Cookies.set("accessToken", r.accessToken, {
-        expires: 7,
-        path: "/",
-        secure: true,
-      });
-      Cookies.set("refreshToken", r.refreshToken, {
-        expires: 30,
-        path: "/",
-        secure: true,
-      });
-      if (r?.urlPath != null) {
-        router.replace(r?.urlPath);
-      }
+  const items: MenuProps['items'] = [
+    {
+      label: 'Alex',
+      key: '1',
+      
     },
-    onError: (e) => {
-      // handleFormError(e as any, form);
-      const error = handleFormError(e as any, form);
-      // @ts-ignore
-      toast.error(error?.message);
+    {
+      label: 'Casidella',
+      key: '2',
+     
     },
-  });
-  const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-    console.log(form.originalData);
-    await mutateAsync(form.originalData);
+    
+    
+      
+     
+  ];
+  const handleMenuClick: MenuProps['onClick'] = (e) => {
+    message.info('Click on menu item.');
+    console.log('click', e);
   };
-  useLayoutEffect(() => {
-    SetFormID("sign-up");
-    SetMobileH4("Create your account");
-    SetButtonText("ADD SINGLE CLIENT CREDIT");
-    SetMobileText("1/6");
-    SetMobileValue(20);
-  }, []);
-  useEffect(() => {
-    if (isPending) {
-      SetIsPending(true);
-    } else {
-      SetIsPending(false);
-    }
-  }, [isPending]);
+  
+  const menuProps = {
+    items,
+    onClick: handleMenuClick,
+  };
+ 
   return (
-    <form
-      onSubmit={onSubmit}
-      id={"option3"}
-      className={
-        " md:mt-10 mb-1 flex flex-col h-fit justify-start items-start  md:px-8 px-[16px] w-full"
-      }
-    >
-      <div
-        className={
-          "mt-6 h-fit flex flex-col justify-start items-start gap-8 w-full "
-        }
-      >
-        <div className={"w-full"}>
-          <div className="w-full flex flex-col">
-            <div className=" pl-5 text-lg">
-              <b>Join With Premier Partner Affiliate Lead Links:</b>
-            </div>
-            <div className="w-full flex flex-row">
-              <div className=" w-5 h-5 "><QuestionMarkCircleIcon /></div>
-              <div className="pl-2">
-                Friends, contacts, acquaintances that could benefit from our products and services?
-              </div>
-            </div>
-            <div className="w-full flex flex-row">
-              <div className=" w-5 h-5 "><QuestionMarkCircleIcon /></div>
-              <div className="pl-2 text-base">
-                Looking to generate more income without building a sales team?
-              </div>
-            </div>
-          </div>
+     <>
+     <section className="w-full h-full flex flex-col items-center justify-center pt-20 ">
+       <div className="w-full h-full flex flex-col items-center justify-start gap-2  ">
+       <div className="text-[#686666] flex w-6/12 items-center justify-between pt-20">
+        Generate the letter to get all credit report
+        <EllipsisOutlined  className="text-white bg-[#1380FF] rounded-full"/>
         </div>
-        <div className={"w-full pt-5 flex flex-row"}>
-          <div className=" text-md text-gray-700 w-[60%]">
-            We Close The Sale, You Make A commission
+        <div className="bg-white w-6/12 h-[800px] shadow-md rounded-lg flex flex-col items-center pt-10">
+          <div className="w-[90%] ">
+          <Dropdown menu={menuProps} className="w-full h-[80px] flex justify-center gap-10 items-center text-[#686666] border-gray-400 rounded-xl border-[1px]">
+        <Button>
+        <Space>
+          <p>Fredrick Jidje</p>
+          <DownOutlined />
+        </Space>
+      </Button>
+         </Dropdown>
           </div>
-          <div className=" text-m pl-6 w-[40%] justify-center content-center text-center text-blue-300">
-            Use The Links Below:
-          </div>
-        </div>
-        <div className={"w-full"}>
-          <div className="flex flex-row w-full contet-center items-center">
-            <div className="w-[1.2rem] h-[3rem] bg-gradient-to-r from-white to-blue-800 rounded-r-full"></div>
-            <div className="w-[1.5rem] h-[1.5rem] bg-blue-700 rounded-full text-center ml-2"><span className="text-white">i</span></div>
-            <div className="w-full ml-1">
-              <p>Important Info</p>
-              <span className="text-sm text-gray-700">
-                Do not book any meeting on behalf of a lead, unless your lead confirmed they can, and will be available. Meethin no-shows will result in your ability to submit leads and earn commissions in being suspended.
-              </span>
+
+          <div className="W-[90%] flex flex-col ">
+            <div className="card w-full h-[80px] bg-[#F9F9F9]">
+        dfsf
             </div>
+
           </div>
+
         </div>
-        <div className={"w-full"}>
-          <div className="flex flex-row w-full contet-center items-center ">
-            <div className="w-[1.2rem] h-[15rem] bg-blue-700 rounded-l-full ml-12"></div>
-            <div className="flex flex-col w-full justify-center content-center items-center">
-              <div className="text-gray-800 text-lg">
-                Schedule Consultation Page Link:
-              </div>
-              <div className="w-[40px] h-[40px] m-6">
-                <img className="w-[40px] h-[40px]" src="/done.png" alt="" />
-              </div>
-              <div className="text-gray-600">
-                https://et128.isrefer.com/go/bcconsult/true
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </form>
+       </div>
+
+     </section>
+     </>
   );
 };
